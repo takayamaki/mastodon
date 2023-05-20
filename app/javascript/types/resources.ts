c883d10bd4a11dea2ad7eafa581d50d709a666d1
takayamaki / fusagiko <24884114+takayamaki@users.noreply.github.com>
2023-05-20 17:31:38 +0900

    replace AccountApiResponseValues with auto generated type annotation

--- app/javascript/types/resources.ts
+++ app/javascript/types/resources.ts
@@ -1,40 +1,6 @@
 import type { Record } from 'immutable';
 
-type CustomEmoji = Record<{
-  shortcode: string;
-  static_url: string;
-  url: string;
-}>;
-
-type AccountField = Record<{
-  name: string;
-  value: string;
-  verified_at: string | null;
-}>;
-
-interface AccountApiResponseValues {
-  acct: string;
-  avatar: string;
-  avatar_static: string;
-  bot: boolean;
-  created_at: string;
-  discoverable: boolean;
-  display_name: string;
-  emojis: CustomEmoji[];
-  fields: AccountField[];
-  followers_count: number;
-  following_count: number;
-  group: boolean;
-  header: string;
-  header_static: string;
-  id: string;
-  last_status_at: string;
-  locked: boolean;
-  note: string;
-  statuses_count: number;
-  url: string;
-  username: string;
-}
+import type { components } from '../../../openapi/lib/mastodon';
 
 type NormalizedAccountField = Record<{
   name_emojified: string;
@@ -50,5 +16,5 @@ interface NormalizedAccountValues {
 }
 
 export type Account = Record<
-  AccountApiResponseValues & NormalizedAccountValues
+  components['schemas']['account'] & NormalizedAccountValues
 >;

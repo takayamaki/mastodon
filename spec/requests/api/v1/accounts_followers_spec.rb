46d2d94354b305c4a254b2129de34235ac493c7b
takayamaki / fusagiko <24884114+takayamaki@users.noreply.github.com>
2023-05-21 17:27:17 +0900

    add request spec for GET /api/v1/accounts/{account_id}/followers

---
+++ spec/requests/api/v1/accounts_followers_spec.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe 'GET /api/v1/accounts/{account_id}/followers' do
+  it 'returns followers of given account order by desc as 200 OK without token' do
+    account = Fabricate(:account)
+    followers = Array.new(2) do
+      follower = Fabricate(:account)
+      Fabricate(:follow, account: follower, target_account: account)
+      follower
+    end
+
+    get "/api/v1/accounts/#{account.id}/followers"
+
+    assert_schema_conform(200)
+    expect(response).to have_http_status(200)
+
+    response_body = body_as_json
+    expect(response_body.pluck(:id)).to match(followers.reverse.map { _1.id.to_s })
+  end
+
+  it 'limits maximum number of results via limit query parameter' do
+    account = Fabricate(:account)
+    followers = Array.new(2) do
+      follower = Fabricate(:account)
+      Fabricate(:follow, account: follower, target_account: account)
+      follower
+    end
+
+    get "/api/v1/accounts/#{account.id}/followers?limit=1"
+
+    assert_schema_conform(200)
+    expect(response).to have_http_status(200)
+
+    response_body = body_as_json
+    expect(response_body.length).to eq(1)
+    expect(response_body.pluck(:id)).to contain_exactly(followers.last.id.to_s)
+  end
+end
